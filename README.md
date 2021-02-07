2a2267348543ac5a52f2c29a57de65bc97f71f0d
SuMu <SuMu31707@163.com>
2021-02-07 23:04:12 +0800

    change

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # SMHP
 简约的个人主页、个人起始页，自适应
+**[点我预览](https://n0ts.cn)**
+![image-20210207221119931](https://gitee.com/SuMu31707/md-img/raw/master/img/blog/image-20210207221119931.png)
+
+## 简介
+**SMHP (SuMu Home Page) 是基于 [NutssssIndex](https://gitee.com/nutssss/NutssssIndex) 做改动的个人主页，非常感谢 [N0ts](https://n0ts.cn/) 前辈的付出，让我有了改动的机会**
+
+起初自己把博客直接部署在一级域名上感觉有点别扭，想弄个人主页，自己又没艺术细胞
+
+于是乎到GitHub上面搜索个人主页，经过层层筛选，感觉 **NutssssIndex** 是我喜欢的风格
+
+由于看到bash窗口为静态，就有了改动的灵感，于是将bash窗口改为动态模拟命令行输入特效
+
+将资料卡标题打字特效改为动态切换特效、资料卡下方按钮改为悬停发光特效，增加动态线条背景
+
+## 使用方法
+这还要看嘛？
+
--- js/card.js
+++ js/card.js
@@ -1,4 +1,4 @@
-var dynamicTitle = function (r) {
+var chakhsu = function (r) {
     function t() {
         return b[Math.floor(Math.random() * b.length)]
     }
@@ -18,11 +18,11 @@ var dynamicTitle = function (r) {
     }
 	
     var l = ""
-        o = ["I'm SuMu!","欢迎访问我的主页~" ].map(function (r) {
+        o = ["I'm SuMu!", "我是苏木！","Welcome to my home page~","欢迎访问我的主页~" ].map(function (r) {
             return r + ""
         }), a = 2, g = 1, s = 5, d = 75,
         b = ["rgb(110,64,170)", "rgb(150,61,179)", "rgb(191,60,175)", "rgb(228,65,157)", "rgb(254,75,131)", "rgb(255,94,99)", "rgb(255,120,71)", "rgb(251,150,51)", "rgb(226,183,47)", "rgb(198,214,60)", "rgb(175,240,91)", "rgb(127,246,88)", "rgb(82,246,103)", "rgb(48,239,130)", "rgb(29,223,163)", "rgb(26,199,194)", "rgb(35,171,216)", "rgb(54,140,225)", "rgb(76,110,219)", "rgb(96,84,200)"],
         c = {text: "", prefixP: -s, skillI: 0, skillP: 0, direction: "forward", delay: a, step: g};
     i()
 };
-dynamicTitle(document.getElementById('meBox-title'));
+chakhsu(document.getElementById('meBox-title'));
